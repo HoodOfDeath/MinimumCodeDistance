2362b94aff41ad96812b785aa589374fd99e998c
KonstantinHoodWB <k.namavir@gmail.com>
2021-03-24 09:17:02 +0300

    First version with multithreading. Calculation distributed evenly between threads.

--- MinimumCodeDistance/src/MinimumCodeDistance.cpp
+++ MinimumCodeDistance/src/MinimumCodeDistance.cpp
@@ -3,90 +3,50 @@
 #include "pch.h"
 #include "Matrix.h"
 #include <chrono>
+#include <vector>
+#include <future>
 
-int Factorial(int value)
+unsigned __int16* _vectorArray;
+unsigned __int16* _identitySubmatrix;
+Matrix* _matrixArray;
+std::vector<std::future<void>> _futures;
+
+void ConstructMatricesRange(const unsigned short lines, const unsigned short columns, const unsigned int startIndex, const unsigned int count) noexcept
 {
-	if (value < 0)
-	{
-		throw std::invalid_argument("Argument of factorial is negative.");
-	}
-
-	int result = 1;
-
-	while (value > 1)
-	{
-		result *= value;
-		--value;
-	}
-
-	return result;
-}
-
-int main()
-{
-	int codeDistance[10] = { 0 };
-	int checked = 0;
-	short lines, columns;
-	short maxCodeDistance = 1;
-
-	std::cout << "Enter code parameters (n k): ";
-	std::cin >> columns >> lines;
-
 	short pSubmatrixColumns(columns - lines);
 
-	auto start = std::chrono::high_resolution_clock::now();
-
-	int vectorsQuantity = (1 << pSubmatrixColumns); //  quantity of unique vectors size of "lines". Equals to (2 power "lines") - 1
+	unsigned int vectorsQuantity = (1 << pSubmatrixColumns); //  quantity of unique vectors size of "pSubmatrixColumns". Equals to (2 power "pSubmatrixColumns") - 1
 	vectorsQuantity--;
 
-	unsigned __int8* vectorArray = new unsigned __int8 [vectorsQuantity];
+	std::vector<unsigned int> linesCombinations;
+	linesCombinations.reserve(lines);
 
-	for (unsigned int i = 1; i <= vectorsQuantity; i++)
+	unsigned int remainder = startIndex;
+
+	for (unsigned short i = 0; i < lines; ++i)
 	{
-		vectorArray[i - 1] = i;
+		linesCombinations.push_back(remainder % vectorsQuantity);
+		remainder = remainder / vectorsQuantity;
 	}
 
-	unsigned __int64 matrixQuantity = 1;
+	const unsigned int endIndex = startIndex + count;
 
-	for (int i = 0; i < lines; i++)
-	{
-		matrixQuantity *= vectorsQuantity;
-	}
-
-
-	Matrix* matrixArray = new Matrix[matrixQuantity];
-
-	for (int i = 0; i < matrixQuantity; i++)
-	{
-		matrixArray[i].SetDimensions(columns, lines);
-	}
-
-	unsigned __int16* identitySubmatrix = new unsigned __int16[lines];
-
-	for (int i = 0; i < lines; ++i)
-	{
-		identitySubmatrix[i] = (1 << (lines - i - 1));
-	}
-
-	short* linesCombinations = new short[lines]; // columnsCombinations[vectorsQuantity][matrixQuantity]
 	// there starts making P submatrix as combinations of precombined vectors
-
-	for (int i = 0; i < lines; i++)
+	for (unsigned int i = startIndex; i < endIndex; ++i)//this loot is iterate through all combinations of vectors
 	{
-		linesCombinations[i] = 0;
-	}
+		_matrixArray[i].SetDimensions(columns, lines);
 
-	for (int i = 0; i < matrixQuantity; i++)//this loot is iterate through all combinations of vectors
-	{
-		for (int lineIndex = 0; lineIndex < lines; ++lineIndex)
+		for (unsigned int lineIndex = 0; lineIndex < lines; ++lineIndex)
 		{
-			unsigned __int16 rowTemp = identitySubmatrix[lineIndex];
-			unsigned short lineVectorIndex = linesCombinations[lineIndex];
-			unsigned __int16 bitValue = vectorArray[lineVectorIndex];
+			unsigned __int16 rowTemp = _identitySubmatrix[lineIndex];
+			unsigned int lineVectorIndex = linesCombinations[lineIndex];
+			unsigned __int16 bitValue = _vectorArray[lineVectorIndex];
 			rowTemp = ((rowTemp << pSubmatrixColumns) + bitValue);
 
-			matrixArray[i].SetRow(rowTemp, lineIndex);
+			_matrixArray[i].SetRow(rowTemp, lineIndex);
 		}
+
+		_matrixArray[i].CalculateMinCodeDistance();
 
 		if (linesCombinations[0] < vectorsQuantity - 1)
 		{
@@ -95,7 +55,7 @@ int main()
 		else
 		{
 			linesCombinations[0] = 0;
-			for (int pLines = 1; pLines < lines; ++pLines)
+			for (unsigned int pLines = 1; pLines < lines; ++pLines)
 			{
 				if (linesCombinations[pLines] < vectorsQuantity - 1)
 				{
@@ -108,15 +68,90 @@ int main()
 				}
 			}
 		}
+	}
+}
 
-		matrixArray[i].CalculateMinCodeDistance();
+int main()
+{
+	unsigned int hardwareThreadsCount = std::thread::hardware_concurrency();
+	
+	if (hardwareThreadsCount == 0)
+	{
+		hardwareThreadsCount = 2;
 	}
 
-	delete[] vectorArray;
+	int codeDistance[10] = { 0 };
+	int checked = 0;
+	unsigned short lines, columns;
+	short maxCodeDistance = 1;
 
-	delete[] identitySubmatrix;
+	std::cout << "Enter code parameters (n k): ";
+	std::cin >> columns >> lines;
 
-	delete[] linesCombinations;
+	short pSubmatrixColumns(columns - lines);
+
+	auto start = std::chrono::high_resolution_clock::now();
+
+	unsigned int vectorsQuantity = (1 << pSubmatrixColumns); //  quantity of unique vectors size of "lines". Equals to (2 power "lines") - 1
+	vectorsQuantity--;
+
+	_vectorArray = new unsigned __int16 [vectorsQuantity];
+
+	for (unsigned int i = 1; i <= vectorsQuantity; ++i)
+	{
+		_vectorArray[i - 1] = i;
+	}
+
+	unsigned int matrixQuantity = 1;
+
+	_identitySubmatrix = new unsigned __int16[lines];
+
+	for (unsigned int i = 0; i < lines; i++)
+	{
+		matrixQuantity *= vectorsQuantity;
+		_identitySubmatrix[i] = (1 << (lines - i - 1));
+	}
+
+	_matrixArray = new Matrix[matrixQuantity];
+
+	const unsigned int baseCount = matrixQuantity / hardwareThreadsCount;
+	const unsigned int remainder = matrixQuantity % hardwareThreadsCount;
+
+	std::vector<unsigned int> matrixPerThread;
+	matrixPerThread.reserve(hardwareThreadsCount);
+
+	for (unsigned int i = 0; i < hardwareThreadsCount; i++)
+	{
+		if (i < remainder)
+		{
+			matrixPerThread.push_back(baseCount + 1);
+		}
+		else
+		{
+			matrixPerThread.push_back(baseCount);
+		}
+	}
+
+	unsigned int startIndex = 0;
+	unsigned int endIndex = 0;
+	
+	_futures.reserve(hardwareThreadsCount);
+
+	for (unsigned int i = 0; i < hardwareThreadsCount; i++)
+	{
+		startIndex = endIndex;
+		endIndex += matrixPerThread[i];
+		_futures.push_back(std::async(std::launch::async, ConstructMatricesRange, lines, columns, startIndex, matrixPerThread[i]));
+	}
+
+	for (unsigned int i = 0; i < hardwareThreadsCount; i++)
+	{
+		_futures[i].wait();
+	}
+
+	delete[] _vectorArray;
+
+	delete[] _identitySubmatrix;
 
 	auto end = std::chrono::high_resolution_clock::now();
 
@@ -129,20 +164,20 @@ int main()
 		std::cout << matrixArray[i] << "\n\n";
 	}*/
 
-	for (int f = 0; f < matrixQuantity; f++)
+	for (unsigned int f = 0; f < matrixQuantity; f++)
 	{
-		if (maxCodeDistance < matrixArray[f].GetCodeDistance())
+		if (maxCodeDistance < _matrixArray[f].GetCodeDistance())
 		{
-			maxCodeDistance = matrixArray[f].GetCodeDistance();
+			maxCodeDistance = _matrixArray[f].GetCodeDistance();
 		}
 	}
 
 	std::cout << "Matrix quantity " << matrixQuantity << "\n";
 	std::cout << "Max code distance is " << maxCodeDistance << "\n";
 
-	for (int i = 0; i < matrixQuantity; i++)
+	for (unsigned int i = 0; i < matrixQuantity; i++)
 	{
-		int D = matrixArray[i].GetCodeDistance();
+		int D = _matrixArray[i].GetCodeDistance();
 
 		codeDistance[D - 1]++;
 	}
